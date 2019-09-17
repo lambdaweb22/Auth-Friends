b9da1331c50512173cc6cbb597bcda6b8433d9e6
Kenna <kennaalastair@gmail.com>
2019-09-17 15:55:57 -0700

    Add new friend form

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,8 +1,9 @@
 import React from "react";
+import axios from 'axios';
 
 class AddFriend extends React.Component {
     state = {
-        friends: {
+        newFriend: {
             id: '',
             name: '',
             age: '',
@@ -10,10 +11,49 @@ class AddFriend extends React.Component {
         }
     };
 
+    handleChange = e => {
+        this.setState({
+            newFriend: {
+                ...this.state.NewFriend,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .post()
+    }
+
     render() {
         return(
-            <>
-            </>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    Name:
+                    <input
+                        type="text"
+                        name="name"
+                        value={this.state.newFriend.name}
+                        onChange={this.handleChange}
+                    />
+                    Age:
+                    <input
+                        type="text"
+                        name="age"
+                        value={this.state.newFriend.age}
+                        onChange={this.handleChange}
+                    />
+                    Email:
+                    <input
+                        type="email"
+                        name="email"
+                        value={this.state.newFriend.email}
+                        onChange={this.handleChange}
+                    />
+                    <button>Add Friend</button>
+                </form>
+            </div>
         )
     }
 }
